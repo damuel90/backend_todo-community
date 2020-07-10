22ec111b248e182a55ce3478949e6bec664162f2
Damuel <damuel90@gmail.com>
2020-07-09 19:32:46 -0430

    [FIXED UP] auth middleware

--- server-dev/middlewares/auth.middleware.js
+++ server-dev/middlewares/auth.middleware.js
@@ -2,18 +2,17 @@ import { verify } from 'jsonwebtoken';
 import { JWT_SECRET } from '../config';
 
 const auth = (req, res, next) => {
-    const token = req.headers['authorization'];
-    if(!token){
-        res.status(401).send({
+    if(!req.headers.authorization){
+        res.status(403).send({
             status: 401,
-            message: 'El token no fue enviado'
+            message: 'No tienes autorización'
         });
-    }
-
+    } 
+    const token = req.headers.authorization.split(' ')[1]; //realizamos el Bearer para obtener el token
     verify(token, JWT_SECRET, function(err, decodedToken){
         if(err){
-            res.status(401).send({
-                status: 401,
+            res.status(403).send({
+                status: 403,
                 message: 'El token es inválido'
             });
         }
--- server/middlewares/auth.middleware.js
+++ server/middlewares/auth.middleware.js
@@ -10,19 +10,19 @@ var _jsonwebtoken = require("jsonwebtoken");
 var _config = require("../config");
 
 const auth = (req, res, next) => {
-  const token = req.headers['authorization'];
-
-  if (!token) {
-    res.status(401).send({
+  if (!req.headers.authorization) {
+    res.status(403).send({
       status: 401,
-      message: 'El token no fue enviado'
+      message: 'No tienes autorización'
     });
   }
 
+  const token = req.headers.authorization.split(' ')[1]; //realizamos el Bearer para obtener el token
+
   (0, _jsonwebtoken.verify)(token, _config.JWT_SECRET, function (err, decodedToken) {
     if (err) {
-      res.status(401).send({
-        status: 401,
+      res.status(403).send({
+        status: 403,
         message: 'El token es inválido'
       });
     }
